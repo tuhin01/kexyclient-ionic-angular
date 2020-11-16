4f4f79cbb8527a2a626599582eb89eb8c97d58dc
tousif007 <tusif.rahman007@gmail.com>
2020-11-17 01:18:40 +0600

    Update market-place-type.page.ts

--- src/app/pages/kexy/market-place-type/market-place-type.page.ts
+++ src/app/pages/kexy/market-place-type/market-place-type.page.ts
@@ -1,19 +1,19 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AlertController, LoadingController, MenuController, NavController } from '@ionic/angular';
-import { BasePage } from '../../basePage';
-import {Storage} from '@ionic/storage';
-import { routeConstants } from 'src/common/routeConstants';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { AlertController, LoadingController, MenuController, NavController } from "@ionic/angular";
+import { BasePage } from "../../basePage";
+import { Storage } from "@ionic/storage";
+import { routeConstants } from "src/common/routeConstants";
 @Component({
-  selector: 'app-market-place-type',
-  templateUrl: './market-place-type.page.html',
-  styleUrls: ['./market-place-type.page.scss'],
+  selector: "app-market-place-type",
+  templateUrl: "./market-place-type.page.html",
+  styleUrls: ["./market-place-type.page.scss"],
 })
 export class MarketPlaceTypePage extends BasePage implements OnInit {
   private readonly params: any;
   private organization_invitations;
-  public marketPlaceType: string = 'restaurant';
+  public marketPlaceType: string = "restaurant";
 
   constructor(
     public router: Router,
@@ -23,7 +23,7 @@ export class MarketPlaceTypePage extends BasePage implements OnInit {
     public loadingCtrl: LoadingController,
     public alertCtrl: AlertController,
     public menu: MenuController,
-    public navCtrl: NavController,
+    public navCtrl: NavController
   ) {
     super(router, route, httpClient, loadingCtrl, alertCtrl, storage, menu, navCtrl);
     if (this.router.getCurrentNavigation().extras.state) {
@@ -37,7 +37,8 @@ export class MarketPlaceTypePage extends BasePage implements OnInit {
     let invitations = this.params.organization_invitations;
     this.organization_invitations = invitations ? invitations : [];
   }
-private nextBtnTapped() {
+
+  async nextBtnTapped() {
     console.log(this.organization_invitations);
     let restaurantIdList = [];
     let distributorIdList = [];
@@ -46,22 +47,17 @@ private nextBtnTapped() {
       this.organization_invitations.forEach((item) => {
         if (item.restaurant_id) restaurantIdList.push(item.restaurant_id);
         if (item.distributor_id) distributorIdList.push(item.distributor_id);
-      })
+      });
     }
 
-
-    if (this.marketPlaceType === 'distributor') {
-     
-      this.navigateTo(routeConstants.KEXY.DISTRIBUTOR_SELECT_TYPE)
-     
+    if (this.marketPlaceType === "distributor") {
+      await this.navigateTo(routeConstants.KEXY.DISTRIBUTOR_SELECT_TYPE);
     }
-    if (this.marketPlaceType === 'restaurant') {
-        this.navigateTo(routeConstants.KEXY.RESTAURANT_TYPE)
+    if (this.marketPlaceType === "restaurant") {
+      await this.navigateTo(routeConstants.KEXY.RESTAURANT_TYPE);
     }
-    if (this.marketPlaceType === 'supplier') {
-
-           this.navigateTo(routeConstants.KEXY.SUPPLIER_SELECT_TYPE)
+    if (this.marketPlaceType === "supplier") {
+      await this.navigateTo(routeConstants.KEXY.SUPPLIER_SELECT_TYPE);
     }
-
   }
 }
