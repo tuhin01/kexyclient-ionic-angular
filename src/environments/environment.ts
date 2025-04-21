3fa90d611781dfe1507eb13fbae4c45b401c5944
Billal Hossain (Tousif) <tuhin.world@gmail.com>
2025-04-21 12:19:35 +0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyARJ_CSe41mdxFrUQzm3R5M0v3npJ0A7pc",
+    apiKey: "",
     authDomain: "kexyapp.firebaseapp.com",
     databaseURL: "https://kexyapp.firebaseio.com",
     projectId: "kexyapp",
     storageBucket: "kexyapp.appspot.com",
-    messagingSenderId: "499842690539"
+    messagingSenderId: ""
   }
 };
 
